d49c269c9fa9afe70c46355741c594f93deba163
ejinaaa <eunjink0423@gmail.com>
2021-05-06 00:25:54 +0900

    feat: Solve the question no.1

--- case01_context-menu/question/q1_js/index.js
+++ case01_context-menu/question/q1_js/index.js
@@ -1,1 +1,26 @@
 // Write Javascript code here!
+
+const $app = document.getElementById('app');
+const contexts = document.querySelectorAll('.context');
+
+const toggleContextMenu = e => {
+  const target = e.target;
+  const isContextShowed = [...contexts].some(
+    $context => $context.style.display === 'block'
+  );
+  const showedContext = [...contexts].find(
+    $context => $context.style.display === 'block'
+  );
+
+  if (isContextShowed) {
+    showedContext.style.display = 'none';
+    showedContext.parentNode.classList.remove('open');
+  }
+
+  if (target.matches('.item') && showedContext !== target.firstElementChild) {
+    target.firstElementChild.style.display = 'block';
+    target.classList.add('open');
+  }
+};
+
+$app.addEventListener('click', toggleContextMenu);
