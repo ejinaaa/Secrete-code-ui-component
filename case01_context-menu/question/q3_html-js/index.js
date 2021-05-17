1949f48143d1f9f6a4fe3d0cd3ce7a07175fda85
ejinaaa <eunjink0423@gmail.com>
2021-05-17 19:00:19 +0900

    feat: Solve the question 3 of context menu

--- case01_context-menu/question/q3_html-js/index.js
+++ case01_context-menu/question/q3_html-js/index.js
@@ -1,4 +1,13 @@
 // Import stylesheets
-import "./style.css";
+// import './style.css'; // 이거 왜 import 안돼는거야????
 
 // Write Javascript code here!
+const items = document.querySelectorAll('details');
+
+document.body.addEventListener('click', e => {
+  const targetEl = e.target;
+  if (targetEl.matches('p')) return;
+  items.forEach(item => {
+    targetEl.parentElement !== item && item.removeAttribute('open');
+  });
+});
